d5602bcbe01d0168d476ed540637aaba3afbeca3
TornovDutta <tornovdutta@gmail.com>
2024-10-17 09:35:49 +0530

    start crating hello world project

---
+++ src/main/java/org/example/firstapplication/FirstApplication.java
@@ -0,0 +1,14 @@
+package org.example.firstapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstApplication.class, args);
+        System.out.println("hello world");
+    }
+
+}
---
+++ src/test/java/org/example/firstapplication/FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.firstapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
